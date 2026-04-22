56410caead07464e9008c8c6ea6f06ec105545d2
SuPuHe <mizin.a.s.2006@gmail.com>
2026-04-22 14:25:38 +0200

    give permission for users that allow them create files

--- srcs/requirements/bonus/ftp/tools/setup.sh
+++ srcs/requirements/bonus/ftp/tools/setup.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 set -e
 
-FTP_USER=$(head -n 1 /run/secrets/user_credentials | cut -d: -f1)
-FTP_PASS=$(head -n 1 /run/secrets/user_credentials | cut -d: -f2)
+parse_creds() {
+    local file=$1
+    local line=$(head -n 1 "$file")
+    echo "$line"
+}
 
-if ! id "$FTP_USER" &>/dev/null; then
-    useradd -m -s /bin/bash "$FTP_USER"
-    echo "$FTP_USER:$FTP_PASS" | chpasswd
-    usermod -aG www-data "$FTP_USER"
-fi
+ADMIN_DATA=$(parse_creds /run/secrets/credentials)
+USER_DATA=$(parse_creds /run/secrets/user_credentials)
 
-echo "FTP Server is starting for user: $FTP_USER"
+USERS=("$ADMIN_DATA" "$USER_DATA")
+
+for CREDS in "${USERS[@]}"; do
+    LOGIN=${CREDS%%:*}
+    PASS=${CREDS#*:}
+
+    if [ -n "$LOGIN" ] && ! id "$LOGIN" &>/dev/null; then
+        echo "Creating FTP user: $LOGIN"
+        useradd -m -s /bin/bash "$LOGIN"
+        echo "$LOGIN:$PASS" | chpasswd
+        usermod -aG www-data "$LOGIN"
+    fi
+done
+
+chown -R www-data:www-data /var/www/wordpress
+chmod -R 775 /var/www/wordpress
+
+echo "FTP Server is starting. Multiple users enabled."
 exec vsftpd /etc/vsftpd.conf
