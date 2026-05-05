d0d759c23325538b0f5f16df95b2b6ab1399a45f
SuPuHe <mizin.a.s.2006@gmail.com>
2026-05-05 09:36:18 +0200

    change filenames

--- srcs/requirements/bonus/static_site/tools/setup.sh
+++ srcs/requirements/bonus/static_site/tools/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
 
-# Replace common listen directives so nginx listens on configured port
 sed -i "s/listen 80;/listen ${STATIC_SITE_PORT};/g" /etc/nginx/sites-available/default || true
 sed -i "s/listen 80 default_server;/listen ${STATIC_SITE_PORT} default_server;/g" /etc/nginx/sites-available/default || true
 sed -i "s/listen \[::\]:80 default_server;/listen [::]:${STATIC_SITE_PORT} default_server;/g" /etc/nginx/sites-available/default || true
